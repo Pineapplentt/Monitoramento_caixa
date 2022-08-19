4c82c8afc47a54c02ee8ee4e7feb151ef40959b5
MattheusGomes <matheus.silva@sptech.school>
2022-08-19 19:14:16 -0300

    script banco

--- Site-v2/src/database/databaseKashPlus.sql
+++ Site-v2/src/database/databaseKashPlus.sql
@@ -1,9 +1,43 @@
 create database databaseKashPlus;
 use databaseKashPlus;
 
-create table usuario (
-	idUsuario int auto_increment primary key,
-    email varchar(100),
-    senha varchar(100)
+
+CREATE TABLE Banco(
+idBanco INT PRIMARY KEY AUTO_INCREMENT
+,nomeEmpresarial VARCHAR(100)
+,cnpj VARCHAR(20)
 );
-insert into usuario values (null, 'vinicius@gmail.com', '1234');
+
+CREATE TABLE Usuario(
+idUsuario INT PRIMARY KEY AUTO_INCREMENT
+,nome VARCHAR(100)
+,cpf VARCHAR(20)
+,email VARCHAR(100)
+,senha VARCHAR(100)
+,cargo CHAR(3)
+,fkBanco INT, FOREIGN KEY(fkBanco) REFERENCES banco(idBanco)
+);
+
+CREATE TABLE Regiao(
+idRegiao INT PRIMARY KEY AUTO_INCREMENT
+,nomeRegiao VARCHAR(200)
+);
+
+CREATE TABLE CaixaEletronico(
+idCaixasEletronico INT PRIMARY KEY AUTO_INCREMENT
+,localizacao VARCHAR(300)
+,fkBanco INT, FOREIGN KEY(fkBanco) REFERENCES banco(idBanco)
+,fkRegiao INT, FOREIGN KEY(fkRegiao) REFERENCES Regiao(fkReiao)
+);
+
+CREATE TABLE Registro(
+idRegistro INT PRIMARY KEY AUTO_INCREMENT
+,fkCaixaEletronico INT, FOREIGN KEY(fkCaixaEletronico) REFERENCES CaixaEletronico(idCaixaEletronico)
+,dataHorario DATETIME
+,consumoCPU DECIMAL(5,2)
+,temperaturaCPU DECIMAL(5,2)
+,consumoRAM DECIMAL(5,2)
+,consumoREDE DECIMAL(5,2)
+);
+
+/* insert into usuario values (null, 'vinicius@gmail.com', '1234'); */
